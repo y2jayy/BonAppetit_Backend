a053468abdf597e30a40dad8cb8afc929ff5b5e9
Jay Yoon <jay@Jays-MacBook-Pro.local>
2015-11-28 15:04:42 -0800

    jy: got route to work with ios app

--- routes/users.js
+++ routes/users.js
@@ -7,8 +7,12 @@ router.get('/', function(req, res, next) {
 });
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/fetchFolloweeReviewsForUser', function(req, res, next) {
+	var data = [
+		{restaurant_name: "Subway", first_name: "Jay", last_name: "Yoon"},
+		{restaurant_name: "The Cheesecake Factory", first_name: "Clara", last_name: "Yoon"}
+	];
+  res.send({data: JSON.parse(JSON.stringify(data))});
 });
 
 module.exports = router;
